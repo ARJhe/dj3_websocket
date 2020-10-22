726f1dcb2d51006d8cebe96dbbecbcd07dda9145
ArJhe <meopkids@gmail.com>
2020-10-22 10:10:58 +0800

    add a demo for rewriting realtime communication of index page

---
+++ redisNwebsocket/chat/routing.py
@@ -0,0 +1,9 @@
+from django.urls import re_path
+
+from . import consumers
+
+websocket_urlpatterns = [
+    # re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer),
+    # "ws://127.0.0.1:8000/ws/chat/room_name/"
+    re_path(r'ws/chat/$', consumers.ChatConsumer),
+]
